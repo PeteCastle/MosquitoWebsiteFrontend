bebb3ad20ca284264337c1bf790bcdef75ae5996
IcedTeaLemon <134358561+IcedTeaLemon@users.noreply.github.com>
2024-03-13 23:07:01 +0800

    miraiza

---
+++ mosquito-website-frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+import Homepage from "./Homepage";
+
+function App() {
+  return (
+
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
